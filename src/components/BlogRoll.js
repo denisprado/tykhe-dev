ea24f20c30334718810b5ac44b10a06114b4c57c
Denis <denisforigo@gmail.com>
2019-12-18 16:51:54 -0300

    working in progress

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,9 +1,8 @@
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { setState } from "react";
 import PropTypes from "prop-types";
 import { Link, graphql, StaticQuery } from "gatsby";
 import PreviewCompatibleImage from "./PreviewCompatibleImage";
+import { kebabCase } from "lodash";
 
 export const BlogRoll = props => {
   const { data } = props;
@@ -51,104 +50,68 @@ export const BlogRoll = props => {
 
   return (
     <>
-      <div className="field">
-        <div className="control has-icons-left has-icons-right">
-          <input
-            className="input is-medium is-primary"
-            type="text"
-            id="filter"
-            placeholder="Pesquise em nossas publicações."
-            onChange={handleInputChange}
-          />
-          <span className="icon is-medium is-left">
-            <i className="fas fa-envelope fa-lg"></i>
-          </span>
-          <span className="icon is-medium is-right">
-            <i className="fas fa-check fa-lg"></i>
-          </span>
+      <nav className="panel margin-bottom-3">
+        <div className="panel-block has-background-grey-lighter">
+          <p className="control has-icons-left">
+            <input
+              className="input"
+              type="text"
+              id="filter"
+              placeholder="Pesquise em nossas publicações."
+              onChange={handleInputChange}
+            />
+            <span className="icon is-left">
+              <i className="fas fa-search" aria-hidden="true"></i>
+            </span>
+          </p>
         </div>
-      </div>
-
-      <div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div
-              className='column card is-4 blog-list-item tile is-child box notification ${
-              post.frontmatter.featuredpost ? "is-featured" : ""'
-              key={post.id}
-            >
-              <div className="card-image ">
-                <figure className="image ">
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                </figure>
-              </div>
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-content">
-                    <p className="title is-4">{post.frontmatter.title} </p>
-                    {/* <p className="subtitle is-6">@johnsmith</p> */}
+      </nav>
+      <div className="container">
+        <div className="columns is-multiline">
+          {posts &&
+            posts.map(({ node: post }) => (
+              <div className="column margin-right-3 is-3" key={post.id}>
+                <div className="card-image">
+                  <figure className="image">
+                    {post.frontmatter.featuredimage ? (
+                      <div className="featured-thumbnail">
+                        <PreviewCompatibleImage
+                          imageInfo={{
+                            image: post.frontmatter.featuredimage,
+                            alt: `Imagem de destaque para ${post.frontmatter.title}`
+                          }}
+                        />
+                      </div>
+                    ) : null}
+                  </figure>
+                </div>
+                <div className="card-content">
+                  <p className="title is-4">{post.frontmatter.title}</p>
+                  <div className="content">
+                    {post.frontmatter.tags && post.frontmatter.tags.length
+                      ? post.frontmatter.tags.map(tag => (
+                          <Link
+                            to={`/tags/${kebabCase(tag)}/`}
+                            key={tag + `tag`}
+                          >
+                            <span className="tag is-medium">{tag}</span>
+                          </Link>
+                        ))
+                      : null}
                   </div>
-                </div>
-
-                <div className="content">
-                  {post.excerpt}
-                  {post.tags && post.tags.map(tag => <a href="#">#{tag} </a>)}
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Continue Lendo →
-                  </Link>
-                  <time datetime="2016-1-1">{post.frontmatter.date}</time>
+                  <div className="content">{post.excerpt}</div>
+                  <footer className="card-footer">
+                    <div className="card-footer-item">
+                      <time dateTime="2016-1-1">{post.frontmatter.date} </time>
+                    </div>
+                    <div className="card-footer-item">
+                      <Link to={post.fields.slug}>Continue Lendo →</Link>
+                    </div>
+                  </footer>
                 </div>
               </div>
-              {/* <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? "is-featured" : ""
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Continue Lendo →
-                  </Link>
-                </p>
-              </article> */}
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
     </>
   );
--- src/pages/publicacoes/index.js
+++ src/pages/publicacoes/index.js
@@ -7,13 +7,24 @@ export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <section className="section">
+        <section className="section container">
+          <nav className="breadcrumb margin-bottom-3" aria-label="breadcrumbs">
+            <ul>
+              <li>
+                <a href="/">TYKHE</a>
+              </li>
+              <li className="is-active">
+                <a aria-current="page" href="/publicacoes">
+                  Publicações
+                </a>
+              </li>
+            </ul>
+          </nav>
           <div className="container">
-            <h1 class="title">Publicações</h1>
-            <h2 class="subtitle">Publicações da TYKHE</h2>
-            <div className="content">
-              <BlogRoll />
-            </div>
+            <h1 className="title">Publicações</h1>
+            <h2 className="subtitle is-spaced">Publicações da TYKHE</h2>
+
+            <BlogRoll />
           </div>
         </section>
       </Layout>
