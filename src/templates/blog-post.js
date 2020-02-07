25cfb830ed3b4c5d3d49d5f3a50b0c2c4a597e5b
denisprado <denisforigo@gmail.com>
2020-02-07 10:50:42 -0300

    better title mobile

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -43,7 +43,7 @@ export const BlogPostTemplate = ({
       <div className="container content">
         <div className="columns">
           <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+            <h1 className="title is-size-2-tablet is-size-6-mobile is-bold-light">
               {title}
             </h1>
             <PostContent content={content} />
