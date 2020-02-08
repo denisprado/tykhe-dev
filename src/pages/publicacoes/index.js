3ba9b60a7da37bb4eb99c89182b3c4c5ebeb2dc5
Denis <denisforigo@gmail.com>
2020-02-08 19:56:16 -0300

    cleaning admin panel

--- src/pages/publicacoes/index.js
+++ src/pages/publicacoes/index.js
@@ -8,25 +8,27 @@ export default class BlogIndexPage extends React.Component {
     return (
       <Layout>
         <section className="section">
-          <div className="container columns">
-            <div className="column is-offset-1">
-              <nav
-                className="breadcrumb margin-bottom-3"
-                aria-label="breadcrumbs"
-              >
-                <ul>
-                  <li>
-                    <a href="/">TYKHE</a>
-                  </li>
-                  <li className="is-active">
-                    <a aria-current="page" href="/publicacoes">
-                      Publicações
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-              <div className="container">
-                <BlogRoll />
+          <div className="container">
+            <div className="columns">
+              <div className="column is-offset-1">
+                <nav
+                  className="breadcrumb margin-bottom-3"
+                  aria-label="breadcrumbs"
+                >
+                  <ul>
+                    <li>
+                      <a href="/">TYKHE</a>
+                    </li>
+                    <li className="is-active">
+                      <a aria-current="page" href="/publicacoes">
+                        Publicações
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+                <div className="container">
+                  <BlogRoll />
+                </div>
               </div>
             </div>
           </div>
